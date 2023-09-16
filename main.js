6a12dd0e235d1bcc784c27ad2ab0b5abe145bef1
Cheryl Tan <cheryltan134@gmail.com>
2023-09-16 17:51:01 +0800

    setting up board

---
+++ main.js
@@ -0,0 +1,77 @@
+/*
+1) Define required constants:
+  1.1) Define a colors object with keys of 'null' (when the square is empty), and players 1 & -1. The value assigned to each key represents the color to display for an empty square (null), player 1 and player -1.
+  1.2) Define the 8 possible winning combinations, each containing three indexes of the board that make a winner if they hold the same player value.
+*/ 
+const colors = {
+    "-1" : "red",
+    "1" : "yellow", 
+    "null" : "white"
+}
+
+/*
+2) Define required variables used to track the state of the game:
+  2.1) Use a board array to represent the squares.
+  2.2) Use a turn variable to remember whose turn it is.
+  2.3) Use a winner variable to represent three different possibilities - player that won, a tie, or game in play.
+*/ 
+var board = [button1, button2, button3, button4, button5, button7,button8, button9] 
+/* need to add this so it's a global variable right? */ 
+var turn = null 
+var winner = null 
+
+const button1 = document.getElementById("button1")
+const button2 = document.getElementById("button2")
+const button3 = document.getElementById("button3")
+const button4 = document.getElementById("button4")
+const button5 = document.getElementById("button5")
+const button6 = document.getElementById("button6")
+const button7 = document.getElementById("button7")
+const button8 = document.getElementById("button8")
+const button9 = document.getElementById("button9")
+
+
+function initialise() {
+    var board = [button1, button2, button3, button4, button5, button7,button8, button9] 
+    var turn = 1 
+    var winner = null 
+}
+
+
+/* 
+  4.2) Render those state variables to the page:
+    4.2.1) Render the board:
+      4.2.1.1) Loop over each of the 9 elements that represent the squares on the page, and for each iteration:
+        4.2.1.1.2) Use the index of the iteration to access the mapped value from the board array.
+        4.3.1.1.3) Set the background color of the current element by using the value as a key on the colors lookup object (constant).
+    4.2.2) Render a message:
+      4.2.2.1) If winner has a value other than null (game still in progress), render whose turn it is - use the color name for the player, converting it to upper case.
+      4.2.2.2) If winner is equal to 'T' (tie), render a tie message.
+      4.2.2.3) Otherwise, render a congratulatory message to which player has won - use the color name for the player, converting it to uppercase.
+  4.3) Wait for the user to click a square
+*/ 
+
+
+
+/* 
+5) Handle a player clicking a square:
+  5.1) Obtain the index of the square that was clicked by either:
+    5.1.1) "Extracting" the index from an id assigned to the element in the HTML, or
+    5.1.2) Looping through the cached square elements using a for loop and breaking out when the current square element equals the event object's target.
+  5.2) If the board has a value at the index, immediately return because that square is already taken.
+  5.3) If winner is not null, immediately return because the game is over.
+  5.4) Update the board array at the index with the value of turn.
+  5.5) Flip turns by multiplying turn by -1 (flips a 1 to -1, and vice-versa).
+  5.6) Set the winner variable if there's a winner:
+    5.6.1) Loop through the each of the winning combination arrays defined.
+    5.6.2) Total up the three board positions using the three indexes in the current combo.
+    5.6.3) Convert the total to an absolute value (convert any negative total to positive).
+    5.6.4) If the total equals 3, we have a winner! Set winner to the board's value at the index specified by the first index in the combo array. Exit the loop.
+  5.7) If there's no winner, check if there's a tie:
+    5.7.1) Set winner to 'T' if there are no more nulls in the board array.
+  5.8) All state has been updated, so render the state to the page (step 4.2).
+		
+
+6) Handle a player clicking the replay button:
+  6.1) Do steps 4.1 (initialize the state variables) and 4.2 (render).
+*/ 
